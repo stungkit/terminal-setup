d6dc3b67bafc8c03277fc18b5d27ab56fcd38873
Smati Tungkitkancharoen <stungkit@gmail.com>
2023-08-15 12:48:09 -0700

    Add more brew setup

--- pre-setup.sh
+++ pre-setup.sh
@@ -4,7 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Oh My Zsh
 # https://github.com/robbyrussell/oh-my-zsh#via-curl
