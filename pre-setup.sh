a03c9b96260e2e60b35292a68ad60c7f7f623fdf
Smati Tungkitkancharoen <stungkit@gmail.com>
2023-08-15 12:32:34 -0700

    Add brew to path

--- pre-setup.sh
+++ pre-setup.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
 
 # Oh My Zsh
 # https://github.com/robbyrussell/oh-my-zsh#via-curl
